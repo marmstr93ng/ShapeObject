5f9bc34e6df65c0a42df7f593db3b13f1fe80a46
marmstr93ng <marmstr93ng@gmail.com>
2018-11-29 17:13:32 +0000

    Add formatter to print + area returns a float

--- shape.py
+++ shape.py
@@ -13,16 +13,16 @@ def traingle_area(base, height):
     return 0.5 * base * height
 
 triangle_obj = Shape("Triangle", "Yellow", traingle_area)
-print(triangle_obj.calc_area(2, 1))
+print("Area: {}".format(triangle_obj.calc_area(2, 1)))
 
 def circle_area(radius):
     return math.pi * (radius ** 2)
         
 circle_obj = Shape("Circle", "Red", circle_area)
-print(circle_obj.calc_area(1))
+print("Area: {}".format(circle_obj.calc_area(1)))
 
 def square_area(height):
-    return height ** 2
+    return float(height ** 2)
         
 square_obj = Shape("Square", "Blue", square_area)
-print(square_obj.calc_area(2))
+print("Area: {}".format(square_obj.calc_area(2)))
